c6754cf9c8f5ee512248b9b10af8b04fade118aa
Yuzo <youzow3@yahoo.co.jp>
2024-06-01 20:53:28 +0900

    You can play the game now

--- minesweeper.py
+++ minesweeper.py
@@ -3,9 +3,9 @@ import random
 from argparse import ArgumentParser
 from icecream import ic
 
-class BoardExplosionErrror(Exception):
-    def __init__(self):
-        super().__init__("Mine was exploded!")
+class BoardExplosionError(Exception):
+    def __init__(self, index : tuple[int, int] = None):
+        super().__init__("Mine was exploded!" if index is None else f"Mine at {index} was exploded!")
 
 class Board:
     class State:
@@ -19,6 +19,7 @@ class Board:
 
         def output(self, n_center = 0, color = False, debug = False):
             output = str(self.n_bomb) if self.is_opened else 'F' if self.is_flag else ' '
+            ic(self.is_opened, output)
 
             if color:
                 n_left = max(0, (n_center - len(output)) // 2)
@@ -44,14 +45,13 @@ class Board:
         
         bomb_map = list(range(size[0] * size[1]))
         random.shuffle(bomb_map)
-        bomb_indexes = list(reversed(list(range(size[0] * size[1]))))[:self.n_bomb]
-        ic(bomb_map)
-        ic(bomb_indexes)
         for y in range(-1, 2):
             for x in range(-1, 2):
-                if not self.is_valid_index((x, y)):
+                if not self.is_valid_index((safe_pos[0] + x, safe_pos[1] + y)):
                     continue
-                bomb_map[self.get_index((x, y))] = -1
+                bomb_map[self.get_index((safe_pos[0] + x, safe_pos[1] + y))] = -1
+        bomb_indexes = list(reversed(sorted(set(bomb_map))))[:n_bomb]
+        ic(bomb_indexes)
 
         self.board = [self.State(bomb_map[i] in bomb_indexes) for i in range(size[0] * size[1])]
         for y in range(size[1]):
@@ -105,9 +105,9 @@ class Board:
         elif self[index].is_opened:
             return False
         elif self[index].is_bomb:
-            raise BoardExplosionError()
+            raise BoardExplosionError(index)
         elif self[index].n_bomb > 0:
-            self.is_opened = True
+            self[index].is_opened = True
         else:
             self[index].is_opened = True
             for xi in range(-1, 2):
@@ -120,9 +120,71 @@ class Board:
 
         self[index].is_flag = not self[index].is_flag
 
-def main():
-    board = Board((10, 10), 3, (2, 2), debug = True) 
-    board.print()
+    def clean(self):
+        non_bomb = 0
+        for y in range(size[1]):
+            for x in range(size[0]):
+                state = self[(x, y)]
+                if state.is_bomb and not state.is_flag:
+                    raise BoardExplosionError((x, y))
+                if not state.is_bomb and state.is_flag:
+                    non_bomb += 1
+        if non_bomb > 0:
+            print(f"You put {non_bomb} flag(s) at non-bomb position")
+
+    def check(self) -> bool:
+        for s in self.board:
+            if not s.is_bomb and not s.is_opened:
+                return False
+        return True
+
+def main(args):
+    size_str = args.size.split(',')
+    size = (int(size_str[0]), int(size_str[1]))
+    n_bomb = args.bomb
+    safe_pos_str = args.position.split(',')
+    safe_pos = (int(safe_pos_str[0]), int(safe_pos_str[1]))
+    debug = args.debug
+    if debug:
+        ic.enable()
+    else:
+        ic.disable()
+
+    ic(size)
+    ic(n_bomb)
+    ic(safe_pos)
+    ic(debug)
+    board = Board(size, n_bomb, safe_pos, debug = debug) 
+    while True:
+        board.print()
+        cmd = input("Enter the command (empty to show help):").split(' ')
+        if cmd[0] == "" or cmd[0] == 'h':
+            print("Command List")
+            print("h           Show this help")
+            print("o [x] [y]   Open the specified position")
+            print("f [x] [y]   Put the flag to the specified position")
+            print("c           Clean bombs")
+        elif cmd[0] == 'o' or cmd[0] == 'f':
+            p = (int(cmd[1]), int(cmd[2]))
+            if not board.is_valid_index(p):
+                print("The position {p} is not valid")
+                continue
+            if cmd[0] == 'o':
+                board.open(p)
+            elif cmd[0] == 'f':
+                board.flag(p)
+        elif cmd[0] == 'c':
+            board.clean()
+            break
+
+        if board.check():
+            break
+    print("Clear!")
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser()
+    parser.add_argument("-s", "--size", default = "10, 10", help = "The board size")
+    parser.add_argument("-b", "--bomb", default = 10, help = "Number of bombs", type = int)
+    parser.add_argument("-p", "--position", default = "5, 5", help = "Safe position (Bombs won't be placed 3x3)")
+    parser.add_argument("-d", "--debug", action = "store_true", help = "Start with debug mode")
+    main(parser.parse_args())
