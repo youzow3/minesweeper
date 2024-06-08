63ea97bda4a3ee410e79fe854934a9101ff835e8
Yuzo <youzow3@yahoo.co.jp>
2024-06-08 17:31:19 +0900

    Add AgentBase class and its subclass

--- agent.py
+++ agent.py
@@ -145,13 +145,146 @@ def main_prototype(args):
     ic(total_loss / (epoch / result_split))
     ic(result_list)
 
+class AgentBase:
+    def __init__(self, discount = 0.9):
+        self.memory = []
+        self.discount = discount
+        self.last_reward = 0
+
+    def action(self, state : env.Board):
+        pass
+
+    def remember(self, board : env.Board, action_index : int, reward : float):
+        state_tensor = self.make_state_tensor(board)
+        reward_tensor = torch.tensor(reward, dtype = torch.float) + self.discount * self.last_reward
+        self.last_reward = reward_tensor
+        self.memory.append([state_tensor, action_index, reward_tensor])
+
+    def train(self):
+        pass
+
+    def reset(self, memory = True):
+        if memory:
+            self.memory.clear()
+        self.last_reward = 0
+
+    def make_state_tensor(self, board : env.Board):
+        flag_tensor = torch.tensor([s.is_flag for s in board.board]).reshape(board.size)
+        bomb_tensor = torch.tensor([s.n_bomb if s.is_opened else 0 for s in board.board]).reshape(board.size)
+        return torch.stack([flag_tensor, bomb_tensor]).to(dtype = torch.float)
+ 
+    def make_loader(self, batch_size = 1, shuffle = False):
+        class __Dataset(torch.utils.data.Dataset):
+            def __getitem__(__self, index):
+                return self.memory[index]
+
+            def __getitems__(__self, indexes):
+                return [self.memory[index] for index in indexes]
+
+            def __len__(__self):
+                return len(self.memory)
+        return torch.utils.data.DataLoader(__Dataset(), batch_size = batch_size, shuffle = shuffle)
+
+class Agent(AgentBase):
+    def __init__(self, board_size, discount, filters = [128, 256, 512, 1024]):
+        super().__init__(discount)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        ic(self.device)
+        self.model = nn.Sequential()
+        _filters = [1] + filters
+        for i in range(len(_filters) - 1):
+            self.model.append(nn.Conv2d(_filters[i], _filters[i + 1], 3, padding = "same"))
+            self.model.append(nn.ReLU())
+        self.model.append(nn.Conv2d(_filters[-1], 1, 1))
+        self.model.append(nn.Flatten())
+        self.model.append(nn.Softmax(dim = -1))
+        self.model.to(self.device)
+        self.optimizer = torch.optim.SGD(self.model.parameters())
+
+    def action(self, state : env.Board):
+        s = super().make_state_tensor(state)[1:2, :, :].to(self.device)
+        self.model.eval()
+        with torch.no_grad():
+            a = torch.multinomial(self.model(s)[0], 1)
+        return a.item()
+
+    def train(self):
+        loss = 0
+        self.optimizer.zero_grad()
+        for x, a, r in self.memory:
+            loss += r.to(self.device) * torch.log(self.model(x[1:2, :, :].to(self.device))[0][a])
+        loss = -loss
+        loss.backward()
+        ic(loss)
+        self.optimizer.step()
+
+def main(args):
+    agent = None
+    size = args.size.split(',')
+    size = (int(size[0]), int(size[1]))
+    n_bomb = args.bomb
+    step_max = size[0] * size[1]
+
+    if args.version == 1:
+        agent = Agent(size, args.discount)
+    else:
+        raise Exception("version is out of range")
+
+    n_clear = 0
+    n_fail = 0
+    split_result = []
+
+    ic(size)
+    ic(n_bomb)
+    ic(step_max)
+    ic(args.discount)
+
+    for i in range(args.epoch):
+        board = env.Board(size, n_bomb, None, color = True)
+        step = 0
+        agent.reset()
+        while True:
+            step += 1
+            a = agent.action(board)
+            n = board.open(board.get_position(a))
+
+            if board.bomb_opened:
+                reward = -step
+                n_fail += 1
+            elif board.check():
+                reward = step_max - step
+                n_clear += 1
+            elif n == 0:
+                reward = 0
+            else:
+                reward = 1
+
+            agent.remember(board, a, float(reward))
+            
+            if board.bomb_opened or board.check():
+                board.print()
+                break
+            elif step == step_max:
+                print("Too many steps")
+                break
+        if (i + 1) % args.split == 0:
+            print(f"{i} n_clear/n_fail={n_clear / n_fail * 100:>.4f}%")
+            split_result.append(n_clear / n_fail)
+        
+        agent.train()
+
+    ic(n_clear, n_fail)
+    ic(f"Last Result n_clear/n_fail={n_clear / n_fail * 100}%")
+    ic(split_result)
+        
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-e", "--epoch", default = 1000, help = "Number of epoch", type = int)
+    parser.add_argument("--split", default = 100, help = "Steps before training", type = int)
     parser.add_argument("-s", "--size", default = "10,10", help = "Board size (x, y)")
     parser.add_argument("-b", "--bomb", default = 10, help = "Number of bombs", type = int)
-    parser.add_argument("--split", default = 100, help = "Result splitting", type = int)
     parser.add_argument("-v", "--version", default = 0, help = "Agent version", type = int)
+    parser.add_argument("-d", "--discount", default = 0.9, help = "Discount rate", type = float)
     args = parser.parse_args()
     if args.version == 0:
         main_prototype(args)
